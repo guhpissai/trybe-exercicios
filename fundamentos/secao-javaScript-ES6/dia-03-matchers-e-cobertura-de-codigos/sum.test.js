d21704ccefda732dad7ce58ddac5d3941c18a0ee
guhpissai <gustavopissai3d@gmail.com>
2022-11-22 18:27:17 -0300

    Segundo teste concluido!

--- fundamentos/secao-javaScript-ES6/dia-03-matchers-e-cobertura-de-codigos/sum.test.js
+++ fundamentos/secao-javaScript-ES6/dia-03-matchers-e-cobertura-de-codigos/sum.test.js
@@ -3,5 +3,8 @@ const sum = require('./sum');
 describe('Testando a funcao sum', () => {
   it('Esperado que o retorno seja 9', () => {
     (expect(sum(4,5)).toBe(9));
-  })
-})
+  });
+  it('Esperado que o retorno seja 0', () => {
+    (expect(sum(0,0)).toBe(0));
+  });
+});
