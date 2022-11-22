0215dd463666ac166f73c54e3bdbcca9c13d3e42
guhpissai <gustavopissai3d@gmail.com>
2022-11-22 18:37:17 -0300

    Quarto teste concluido!

--- fundamentos/secao-javaScript-ES6/dia-03-matchers-e-cobertura-de-codigos/sum.test.js
+++ fundamentos/secao-javaScript-ES6/dia-03-matchers-e-cobertura-de-codigos/sum.test.js
@@ -1,3 +1,5 @@
+// Exercicio 1
+
 const sum = require('./sum');
 
 describe('Testando a funcao sum', () => {
@@ -7,7 +9,11 @@ describe('Testando a funcao sum', () => {
   it('Esperado que o retorno seja 0', () => {
     (expect(sum(0,0)).toBe(0));
   });
+  it('Espera que lance um erro', () => {
+    expect(() => sum(4, '5').toThrow(Error));
+  });
   it('Espera que lance um erro: parameters must be numbers', () => {
     expect(() => sum(4, '5').toThrow(Error('parameters must be numbers')));
   });
 });
+
