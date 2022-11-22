363f3f8b322c82086442cdeed22683956949003d
guhpissai <gustavopissai3d@gmail.com>
2022-11-22 19:05:07 -0300

    Testes exercicio 3 concluido

--- fundamentos/secao-javaScript-ES6/dia-03-matchers-e-cobertura-de-codigos/printMessage.test.js
+++ fundamentos/secao-javaScript-ES6/dia-03-matchers-e-cobertura-de-codigos/printMessage.test.js
@@ -4,4 +4,10 @@ describe('Testa a funcao printMessage', () => {
   it('Espera que o objeto passado possua a propriedade personagem', () => {
     expect(info).toHaveProperty('personagem');
   });
+  it('Espera que a resposta contenha "Boas vindas"', () => {
+    expect(printMessage(info)).toMatch(/Boas vindas/);
+  });
+  it('Espera que a resposta tenha o nome correto do persongagem', () => {
+    expect(printMessage(info)).toMatch(/Margarida/);
+  })
 });
