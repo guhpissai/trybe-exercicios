e67b13a3ca981663ac0f31d0df51a8b6e6d41414
guhpissai <gustavopissai3d@gmail.com>
2022-11-29 22:01:37 -0300

    Encontrar pessoa

--- fundamentos/secao-08-Higher-Order-Functions/lottery.js
+++ fundamentos/secao-08-Higher-Order-Functions/lottery.js
@@ -1,5 +1,7 @@
 const numberChecker = (myNumber, number) => myNumber === number;
 
+console.log(numberChecker)
+
 const lotteryResult = (myNumber, callback) => {
   const number = Math.floor((Math.random() * 5) + 1);
 
